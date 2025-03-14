1a915396df5a583336961dcde753b29e46cc2e29
Ahmad Raza <ahmadrazawebexpert@gmail.com>
2025-03-15 00:29:42 +0500

    conditional statements chapter completed.

--- chp-2_CONDITIONAL_STATEMENTS_&_LOOPS/conditional_statements.cpp
+++ chp-2_CONDITIONAL_STATEMENTS_&_LOOPS/conditional_statements.cpp
@@ -48,14 +48,10 @@ int main()
         cout << "Please enter a valid letter!\n";
     }
 
-    /*
-    Ternary Operators:
-    This is another way of implementing if and else-if conditional statements.
-    In this example we will check wether the given input is a postive or a negative
-    ineteger.
-    NOTE: characters (a-z or A-Z) will also be considered in the condition as per ASCII
-    We discussed before.
-    */
+    // In this example we will check wether the given input is a postive or a negative
+    //    ineteger.
+    // NOTE: characters (a-z or A-Z) will also be considered in the condition as per ASCII
+    // We discussed before.
     int number;
     cout << "Please enter a positive or negative number:\n";
     cin >> number;
@@ -71,5 +67,29 @@ int main()
     {
         cout << "Please enter a valid integer value!\n";
     }
+
+    /*
+    Ternary Operators:
+    This is another way of implementing if and else-if conditional statements.
+    In this example we will check if the value of given integer is greater than hundred
+    or not.
+    */
+
+    // 1. if-else
+    int _value;
+    cout << "Please enter a number between 0 to n\n";
+    cin >> _value;
+    _value > 100 ? cout << "The given number is greater than 100\n" : cout << "The given number is less than 100\n";
+
+    // 2. else-if
+    cout
+        << "Please enter a value between 0 to n\n";
+    int _value2;
+    cin >> _value2;
+    _value2 > 100 ? cout << "The given number is greater than 100\n" : _value2 == 100 ? cout << "The value is exactly 100\n"
+                                                                   : _value2 < 0      ? cout << "The given value is incorrect\n"
+                                                                                      : cout << "The given number is less than 100\n";
+
+    // I hope that this was a clear example to understand the short-hand if-else statements.
     return 0;
 }
